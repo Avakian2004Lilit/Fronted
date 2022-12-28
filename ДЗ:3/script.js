905e212a897a15c9ad7e732b1c5e6224c0d51907
Avakian2004Lilit <114296828+Avakian2004Lilit@users.noreply.github.com>
2022-12-28 19:15:32 +0100

    Add files via upload

---
+++ ДЗ:3/script.js
@@ -0,0 +1,111 @@
+//Задача №1
+
+/*function reverseString(str) {
+    var a = "";
+    for (let i = 0; i < str.length; i++) {
+        a += str[(str.length -1) -i];
+        
+    }
+    return a;
+    
+}
+ console.log(reverseString("word"));*/
+
+
+
+ // Задача №2
+ // шалаш
+ // цветок
+ /*function palindrome(str) {
+    return str === str.split('').reverse().join('');    
+ }
+ console.log(palindrome('шалаш'));
+ console.log(palindrome('цветок'))*/
+
+
+// Задача №3
+
+const num = 0;
+for (let i = 0; i < 50; i++) {
+   console.log(num);
+    
+}
+
+
+
+
+// Задача №4
+ /*const product = [
+    {
+      title: "Adidas" ,
+      price: 250,
+      type: "jacket" 
+    },
+    {
+        title: "Nike",
+        price: 100,
+        type: "shoe"
+    },
+    {
+        title: "Audi",
+        price: 10000,
+        type: "car"
+    },
+ ];
+for (let i = 0; i < product.length; i++) {
+    console.log(product[i].title);
+    
+}
+
+ //Задача №5
+ /*const product = [
+    {
+      title: "Adidas" ,
+      price: 250,
+      type: "jacket" 
+    },
+    {
+        title: "Nike",
+        price: 100,
+        type: "shoe"
+    },
+    {
+        title: "Audi",
+        price: 10000,
+        type: "car"
+    },
+ ];
+ for (let i = 0; i < product.length; i++) {
+    console.log(`${product[i].title},price is ${product[i].price}`);
+    
+    
+ }*/
+//Задача №6
+ /*const product = [
+    {
+      title: "Adidas" ,
+      price: 250,
+      type: "jacket" 
+    },
+    {
+        title: "Nike",
+        price: 100,
+        type: "shoe"
+    },
+    {
+        title: "Audi",
+        price: 10000,
+        type: "car"
+    },
+ ];
+
+ for (let i = 0; i < product.length; i++) {
+    if (product[i].price >300) console.log(product[i].title);
+        
+    }*/
+    
+ 
+ 
+
+
+ 
