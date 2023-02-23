46377b62feb19eda617c7d82e2a60eb8c860c70c
Avakian2004Lilit <114296828+Avakian2004Lilit@users.noreply.github.com>
2023-02-23 20:47:28 +0100

    Add files via upload

---
+++ Hw-12/script.js
@@ -0,0 +1,18 @@
+
+
+let input = document.querySelector("input");
+const btnElem = document.querySelector(".elem");
+const btnItem = document.querySelector("item");
+const p = document.querySelector("p");
+
+btnElem.onclick = () => {
+    input.value = input.value -1;
+    localStorage.setItem("number, input.value");
+};
+
+btnItem.onclick = () => {
+    input.value = +input.value +1;
+    localStorage.setItem("number, input.value");
+};
+
+input.value = localStorage.getItem("number");
